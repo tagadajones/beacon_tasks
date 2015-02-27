a127763b440ddb7be3721cda4bd2975a1eab834e
tagadajones <julianieddu@gmail.com>
2015-02-27 16:45:21 +0100

    Create encrypt function

--- task1.py
+++ task1.py
@@ -6,3 +6,14 @@ from string import ascii_uppercase
 #encrypted and uncrypted characters
 encrypted = ascii_uppercase
 plain = encrypted[3:]+encrypted[:3] #Shift list by 4
+
+def encrypt(text):
+	"""Caesar cipher encrypting
+	text : plain text to encrypt
+	returns : encrypted text
+	"""
+	t = []
+	for l in text.upper():
+		if l in plain:	t.append(encrypted[plain.index(l)])		#encrypt letter if it's not punctuation 
+		else:			t.append(l)								#else: append punctuation
+	return  ''.join(t)
