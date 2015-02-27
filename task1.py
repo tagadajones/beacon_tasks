4b4c7d1b1137774f6c0686094afe1f56109bad24
tagadajones <julianieddu@gmail.com>
2015-02-27 16:39:13 +0100

    Two alphabets
    create two lists with encrypted and plain alphabets

--- task1.py
+++ task1.py
@@ -1,2 +1,8 @@
 #!/usr/bin/env python
 #Task 1 : Caesar Cipher
+
+from string import ascii_uppercase
+
+#encrypted and uncrypted characters
+encrypted = ascii_uppercase
+plain = encrypted[3:]+encrypted[:3] #Shift list by 4
