327190280f3aec9e632971a0185fedbf8b375028
tagadajones <julianieddu@gmail.com>
2015-03-06 15:51:36 +0100

    Add encrypt & decrypt functions

--- task3.py
+++ task3.py
@@ -1,8 +1,36 @@
 import BaseHTTPServer
 import SimpleHTTPServer
+from string import ascii_uppercase
 
 FILE = 'task2.html'
 PORT = 8080
+
+#encoded and decoded lists
+encrypted = ascii_uppercase
+plain = encrypted[3:] + encrypted[:3]
+
+def encrypt(text):
+    """Caesar cipher encrypting
+    text : plain text to encrypt
+    returns : encoded text
+    """
+    t = []
+    for l in text.upper():
+        if l in plain:  t.append(encrypted[plain.index(l)])     #decrypt letter if it's not punctuation 
+        else:           t.append(l)                             #else: keep punctuation
+    return  ''.join(t)
+
+def decrypt(text):
+    """Caesar cipher decrypting
+    text : encrypted text to decrypt
+    returns : decrypted text
+    """
+    t = []
+    for l in text.upper():
+        if l in plain:  t.append(plain[encrypted.index(l)])     #decrypt letter if it's not punctuation 
+        else:           t.append(l)                             #else: keep punctuation
+    return  ''.join(t)
+
 
 class Handler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     """Handler - handle post request from task2.html"""
